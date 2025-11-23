72011f00005fdf4c9668ff915b69e7e234c7c8b6
Ubuntu <ubuntu@ip-172-31-29-165.ap-northeast-1.compute.internal>
2025-11-23 11:00:56 +0000

    検索機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   has_one_attached :video_file
 
   validates :content, presence: true
+
+  scope :search_by_keyword, -> (keyword) {
+    unless keyword.blank?
+      where('content LIKE ?', "%#{keyword}%")
+    end
+  }
 end
